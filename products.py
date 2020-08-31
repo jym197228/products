075aa8495a7db856cf40f66974fa3b49500aec9b
Chris Chung <jym197@gmail.com>
2020-08-31 17:18:42 +0800

    add for loop

--- products.py
+++ products.py
@@ -4,7 +4,9 @@ while True:
     if name == 'q':
     	break
     price = input('請輸入商品的價格: ')
-    products.append([name, price]) #你也可以創在全新的清單，然後一一存入name和price
+    products.append([name, price]) 
+    #你也可以創在全新的清單，然後一一存入name和price
 print(products)
 
-products[0][0]
+for p in products:
+	print(p[0], '的價格是', p[1])
